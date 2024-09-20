9c0b814b991cfe3d9107d81bd9a072384ecde688
User <grishkoshkarta1@gmail.com>
2024-09-20 13:20:06 +0300

    finish

--- src/components/dashboard/components/kps/index.tsx
+++ src/components/dashboard/components/kps/index.tsx
@@ -1,9 +1,82 @@
 import { FC } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { SvgInfoIcon } from '../../../../assets/icons';
-import chart from '../../../../assets/imgs/chart2.webp';
 import s from './style.module.scss';
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 const KPS: FC = () => {
+	const data = {
+		labels: ['KP 1', 'KP 2', 'KP 3', 'KP 4', 'KP 5', 'KP 6'],
+		datasets: [
+			{
+				label: 'KP 1',
+				data: [75, null, null, null, null, null],
+				backgroundColor: '#4D5ABF',
+				borderRadius: 6,
+				// barThickness: 76,
+			},
+			{
+				label: 'KP 2',
+				data: [null, 65, null, null, null, 35],
+				backgroundColor: '#5D78C3',
+				borderRadius: 6,
+				// barThickness: 76,
+			},
+			{
+				label: 'KP 3',
+				data: [null, null, 30, null, null, null],
+				backgroundColor: '#6D97C7',
+				borderRadius: 6,
+				// barThickness: 76,
+			},
+			{
+				label: 'KP 4',
+				data: [null, null, null, 60, null, null],
+				backgroundColor: '#7DB5CB',
+				borderRadius: 6,
+				// barThickness: 76,
+			},
+			{
+				label: 'KP 5',
+				data: [null, null, null, null, 40, null],
+				backgroundColor: '#8ED4CF',
+				borderRadius: 6,
+				// barThickness: 30,
+			},
+			{
+				label: 'KP 6',
+				data: [null, null, null, null, null, 20],
+				backgroundColor: '#A0A0A0',
+				borderRadius: 6,
+				// barThickness: 76,
+			},
+		],
+	};
+
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true,
+				max: 100,
+				ticks: {
+					stepSize: 25,
+					callback: (value) => `${value}%`,
+				},
+			},
+		},
+		plugins: {
+			legend: {
+				display: true,
+				position: 'bottom',
+				labels: {
+					boxWidth: 12,
+				},
+			},
+		},
+	};
+
 	return (
 		<div className={s.wrapper}>
 			<div className={s.header}>
@@ -11,7 +84,7 @@ const KPS: FC = () => {
 				<SvgInfoIcon />
 			</div>
 			<div className={s.body}>
-				<img className={s.chart} src={chart} alt="chart" />
+				<Bar data={data} options={{ ...options, responsive: true, maintainAspectRatio: false }} />
 			</div>
 		</div>
 	);
--- src/components/dashboard/components/surveyTopicsScore/index.tsx
+++ src/components/dashboard/components/surveyTopicsScore/index.tsx
@@ -1,9 +1,49 @@
 import { FC } from 'react';
+import { Bar } from 'react-chartjs-2';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend } from 'chart.js';
 import { SvgInfoIcon } from '../../../../assets/icons';
-import chart from '../../../../assets/imgs/chart1.webp';
 import s from './style.module.scss';
 
+ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend);
+
 const SurveyTopicsScore: FC = () => {
+	const data = {
+		labels: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L'],
+		datasets: [
+			{
+				label: 'Values',
+				data: [4.5, 2, 5, 1, 5, 3.5, 4.2, 1, 1.8, 2.3, 0.5, 5],
+				backgroundColor: '#F18C5C',
+				borderColor: '#F18C5C',
+				borderWidth: 1,
+				borderRadius: 6,
+				barThickness: 15,
+			},
+		],
+	};
+
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true,
+				min: 1,
+				max: 5,
+				ticks: {
+					stepSize: 1,
+				},
+			},
+			x: {
+				categoryPercentage: 0.8,
+				barPercentage: 0.9,
+			},
+		},
+		plugins: {
+			legend: {
+				display: false,
+			},
+		},
+	};
+
 	return (
 		<div className={s.wrapper}>
 			<div className={s.header}>
@@ -11,7 +51,7 @@ const SurveyTopicsScore: FC = () => {
 				<SvgInfoIcon />
 			</div>
 			<div className={s.body}>
-				<img className={s.chart} src={chart} alt="cahrt" />
+				<Bar data={data} options={{ ...options, responsive: true, maintainAspectRatio: false }} />
 			</div>
 		</div>
 	);
